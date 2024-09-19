5889bef63d6ce718ba5302072df7133cc1198ba5
Raxskle <3328147378@qq.com>
2024-09-19 15:52:49 +0800

    feat: 重启项目

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,6 @@ import { useRoute } from "vue-router";
 
 const route = useRoute();
 const key = computed(() => route.path + Math.random());
-
-// console.log("route in app", route.path);
-// alert("网站更新中，部分功能暂未能正常使用，还没找到好看的新icon。2023/6/21");
 </script>
 
 <style scoped lang="scss"></style>
